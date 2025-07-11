3817ec5d3ea1ba9f42ffc067aa49ef2b3f5eddc9
Daniel <daniel.kordel@gmail.com>
2025-07-11 13:12:07 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3xl font-bold text-blue-600">Hello, Tailwind!</h1>
